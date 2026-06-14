a5f207ba87655f5378b9b9bd265ee8d1a931d5a3
berawaldarikeresahan <berawaldarikeresahan@gmail.com>
2026-06-14 09:40:57 +0700

    about.py

---
+++ about.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+st.title("ℹ️ About Project")
+
+st.markdown("""
+## Business Problem
+
+HR teams often struggle to identify
+employees eligible for promotion.
+
+## Objective
+
+Predict employee promotion eligibility
+using Machine Learning.
+
+## Model
+
+Gradient Boosting Classifier
+
+## Dataset
+
+Human Capital Dataset
+
+## Tools
+
+- Python
+- Scikit-Learn
+- Streamlit
+- Plotly
+""")
