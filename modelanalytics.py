db18f8fc4dc74ff33081c9f8fd859b0da2284af1
berawaldarikeresahan <berawaldarikeresahan@gmail.com>
2026-06-14 09:40:26 +0700

    Create modelanalytics.py

---
+++ modelanalytics.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+st.title("📈 Model Performance")
+
+c1,c2,c3,c4 = st.columns(4)
+
+with c1:
+    st.metric(
+        "Accuracy",
+        "94%"
+    )
+
+with c2:
+    st.metric(
+        "Precision",
+        "92%"
+    )
+
+with c3:
+    st.metric(
+        "Recall",
+        "90%"
+    )
+
+with c4:
+    st.metric(
+        "ROC-AUC",
+        "95%"
+    )
+
+st.image(
+    "assets/confusion_matrix.png"
+)
