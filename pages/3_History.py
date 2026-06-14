b5af6d0485cc59fc4c4ab8a5c2d7275638ee6c12
berawaldarikeresahan <berawaldarikeresahan@gmail.com>
2026-06-14 10:35:33 +0700

    Update 3_History.py

--- pages/3_History.py
+++ pages/3_History.py
@@ -1,32 +1,3 @@
 import streamlit as st
-import pandas as pd
 
-st.title("📜 Prediction History")
-
-try:
-
-    df = pd.read_csv(
-        "history.csv"
-    )
-
-    st.dataframe(
-        df,
-        use_container_width=True
-    )
-
-    csv = df.to_csv(
-        index=False
-    )
-
-    st.download_button(
-        "Download CSV",
-        csv,
-        "history.csv",
-        "text/csv"
-    )
-
-except:
-
-    st.warning(
-        "No prediction history found"
-    )
+st.title("History")
