78269397ef4396d00f3ccfa96c627982315db7b0
berawaldarikeresahan <berawaldarikeresahan@gmail.com>
2026-06-14 10:13:01 +0700

    Update history_manager.py

--- utils/history_manager.py
+++ utils/history_manager.py
@@ -1,36 +1,22 @@
 import pandas as pd
 from datetime import datetime
 
-def save_history(
-    department,
-    probability
-):
+def save_history(department, probability):
 
     try:
-
-        df = pd.read_csv(
-            "history.csv"
-        )
+        df = pd.read_csv("history.csv")
 
     except:
-
         df = pd.DataFrame()
 
-    new_data = pd.DataFrame([{
-
-        "Timestamp":
-        datetime.now(),
-
-        "Department":
-        department,
-
-        "Probability":
-        round(probability*100,2)
-
+    new_row = pd.DataFrame([{
+        "Timestamp": datetime.now(),
+        "Department": department,
+        "Probability": probability
     }])
 
     df = pd.concat(
-        [df,new_data],
+        [df, new_row],
         ignore_index=True
     )
 
